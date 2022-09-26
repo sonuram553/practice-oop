6aed22dd03a3677e13bd9185418d1f79be36f61f
Sonu Ram <sonuram6960@gmail.com>
2022-09-26 23:48:40 +0530

    Add methods to Tile

--- src/garys-game/tile.ts
+++ src/garys-game/tile.ts
@@ -1,8 +1,21 @@
 import Unit from "./unit";
 
 export default class Tile {
-  addUnit(unit: Unit) {}
-  removeUnit(unit: Unit) {}
-  removeUnits() {}
-  getUnits(){}
+  private units: Unit[] = [];
+
+  addUnit(unit: Unit) {
+    this.units.push(unit);
+  }
+
+  removeUnit(unit: Unit) {
+    this.units = this.units.filter((_unit) => _unit !== unit);
+  }
+
+  removeUnits() {
+    this.units = [];
+  }
+
+  getUnits(): Unit[] {
+    return this.units;
+  }
 }
