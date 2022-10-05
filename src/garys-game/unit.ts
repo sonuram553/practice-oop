cb65a14ea5d2fa907b397f605f394273e1dc7b6f
Sonu Ram <sonuram6960@gmail.com>
2022-10-05 22:40:17 +0530

    Add methods to Unit class

--- src/garys-game/unit.ts
+++ src/garys-game/unit.ts
@@ -1,1 +1,49 @@
-export default class Unit {}
+interface Weapon {}
+
+export default class Unit {
+  private type: string;
+  private name: string;
+  private weapons: Weapon[];
+  private properties: { [key: string]: Object };
+
+  constructor(private id: number) {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  getType(): string {
+    return this.type;
+  }
+
+  setType(type: string) {
+    this.type = type;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  addWeapon(weapon: Weapon) {
+    if (!this.weapons) this.weapons = [];
+    this.weapons.push(weapon);
+  }
+
+  getWeapons(): Weapon[] {
+    return this.weapons;
+  }
+
+  setProperty(property: string, value: Object) {
+    if (!this.properties) this.properties = {};
+    this.properties[property] = value;
+  }
+
+  getProperty(property: string): Object | null {
+    if (!this.properties) return null;
+    return this.properties[property];
+  }
+}
