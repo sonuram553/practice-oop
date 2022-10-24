5d9c0f9c773958a6887373e7b7c6a52fd609a234
Sonu Ram <sonuram6960@gmail.com>
2022-10-25 00:55:09 +0530

    Add assertions

--- src/subway/loader-tester.ts
+++ src/subway/loader-tester.ts
@@ -4,5 +4,17 @@ import SubwayLoader from "./subway-loader";
   const subwayLoader = new SubwayLoader();
   const subway = await subwayLoader.loadFromFile("./subway.txt");
 
-  console.log(subway);
+  if (subway) {
+    console.assert(subway.hasStation("Head First Lounge"));
+    console.assert(subway.hasStation("Ajax Rapids"));
+    console.assert(subway.hasStation("JavaBeans Boulevard"));
+    console.assert(subway.hasStation("ABC") === false);
+
+    console.assert(
+      subway.hasConnection("Ajax Rapids", "HTML Heights", "Booch Line")
+    );
+    console.assert(
+      subway.hasConnection("HTML Heights", "Ajax Rapids", "Booch Line")
+    );
+  }
 })();
